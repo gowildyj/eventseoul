34782655e8e578c16c720d8694b25354df630c76
Stella Chung <gowildyj@gmail.com>
2023-05-09 09:35:24 +0900

    add class name(진행중)

--- script.js
+++ script.js
@@ -127,3 +127,26 @@ fetch(apiUrl + serviceKey + "/" + endPoint)
     eventLocationFilter.addEventListener("change", updateEventList);
   })
   .catch((error) => console.error(error));
+
+/* 맨 위로 버튼 */
+  var scrollToTopBtn = document.getElementById("scroll-to-top");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }  
